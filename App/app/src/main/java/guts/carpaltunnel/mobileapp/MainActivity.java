c22dd538de71724c366cc537dc6bab520bdd8daa
Alistair Miles <ajrmiles@gmail.com>
2017-10-27 21:56:01 +0100

    Hello World commit

---
+++ App/app/src/main/java/guts/carpaltunnel/mobileapp/MainActivity.java
@@ -0,0 +1,13 @@
+package guts.carpaltunnel.mobileapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
