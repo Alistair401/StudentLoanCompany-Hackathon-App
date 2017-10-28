37fc1cca2b6b9a8b315b1d0e3c7817179956a1f0
Alistair Miles <ajrmiles@gmail.com>
2017-10-28 10:57:43 +0100

    Merged signature and facebook info requests

--- App/app/src/main/java/guts/carpaltunnel/mobileapp/UserDetailsActivity.java
+++ App/app/src/main/java/guts/carpaltunnel/mobileapp/UserDetailsActivity.java
@@ -1,13 +1,10 @@
 package guts.carpaltunnel.mobileapp;
 
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.facebook.AccessToken;
@@ -16,67 +13,69 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
-import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
-public class FacebookDemo extends AppCompatActivity {
+public class UserDetailsActivity extends AppCompatActivity {
+
     CallbackManager callbackManager;
+
+    EditText full_name;
+    EditText date_of_birth;
+    EditText email;
+    EditText permanent_address;
+    Button submit_form;
+    LoginButton facebook_login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // DEBUG -- prints out the hashkey for the app, which can be registered with facebook
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(
-                    "guts.carpaltunnel.mobileapp",
-                    PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            }
-        } catch (PackageManager.NameNotFoundException ignored) {
 
-        } catch (NoSuchAlgorithmException ignored) {
-
-        }
-
-        setContentView(R.layout.facebook_form);
         callbackManager = CallbackManager.Factory.create();
 
-        if (AccessToken.getCurrentAccessToken() != null){
-            makeAndFillRequest(AccessToken.getCurrentAccessToken());
-        }
+        setContentView(R.layout.activity_user_details);
 
-        final LoginButton loginButton = findViewById(R.id.login_button);
-        loginButton.setReadPermissions(Arrays.asList("email", "user_birthday"));
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        full_name = findViewById(R.id.full_name);
+        date_of_birth = findViewById(R.id.date_of_birth);
+        email = findViewById(R.id.email);
+        permanent_address = findViewById(R.id.permanent_address);
+        facebook_login_button = findViewById(R.id.facebook_login_button);
+        submit_form = findViewById(R.id.submit_form);
+
+        facebook_login_button.setReadPermissions(Arrays.asList("email", "user_birthday"));
+        facebook_login_button.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 AccessToken accessToken = AccessToken.getCurrentAccessToken();
-                makeAndFillRequest(accessToken);
+                facebookAutofill(accessToken);
             }
 
             @Override
             public void onCancel() {
-                // App code
             }
 
             @Override
             public void onError(FacebookException error) {
-
             }
 
         });
 
+        if (AccessToken.getCurrentAccessToken() != null) {
+            facebookAutofill(AccessToken.getCurrentAccessToken());
+        }
+
+
+        submit_form.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), SignatureActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
     }
 
     @Override
@@ -85,7 +84,7 @@ public class FacebookDemo extends AppCompatActivity {
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
-    private void makeAndFillRequest(AccessToken token){
+    private void facebookAutofill(AccessToken token) {
         GraphRequest request = GraphRequest.newMeRequest(
                 token,
                 new GraphRequest.GraphJSONObjectCallback() {
@@ -97,7 +96,7 @@ public class FacebookDemo extends AppCompatActivity {
                         EditText date_of_birth = findViewById(R.id.date_of_birth);
                         EditText email = findViewById(R.id.email);
 
-                        if (response.getError() == null){
+                        if (response.getError() == null) {
                             try {
                                 full_name.setText(response.getJSONObject().getString("name"));
                                 date_of_birth.setText(response.getJSONObject().getString("birthday"));
@@ -110,8 +109,11 @@ public class FacebookDemo extends AppCompatActivity {
                     }
                 });
         Bundle parameters = new Bundle();
-        parameters.putString("fields","name,birthday,email");
+        parameters.putString("fields", "name,birthday,email");
         request.setParameters(parameters);
         request.executeAsync();
     }
+
+
 }
+
--- App/app/src/main/java/guts/carpaltunnel/mobileapp/WelcomeActivity.java
+++ App/app/src/main/java/guts/carpaltunnel/mobileapp/WelcomeActivity.java
@@ -1,40 +1,39 @@
 package guts.carpaltunnel.mobileapp;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class CaptureSignatureActivity extends Activity {
+public class WelcomeActivity extends AppCompatActivity {
 
     public static final int SIGNATURE_ACTIVITY = 1;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_capture_signature);
+        setContentView(R.layout.activity_welcome);
 
-        Button getSignature = (Button) findViewById(R.id.signature);
-        getSignature.setOnClickListener(new View.OnClickListener() {
+        Button form_button = findViewById(R.id.form_button);
+        form_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent intent = new Intent(CaptureSignatureActivity.this, CaptureSignature.class);
-                startActivityForResult(intent,SIGNATURE_ACTIVITY);
+                Intent myIntent = new Intent(view.getContext(), UserDetailsActivity.class);
+                startActivityForResult(myIntent, 0);
             }
         });
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        switch(requestCode) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
             case SIGNATURE_ACTIVITY:
                 if (resultCode == RESULT_OK) {
 
                     Bundle bundle = data.getExtras();
-                    String status  = bundle.getString("status");
-                    if(status.equalsIgnoreCase("done")){
+                    String status = bundle.getString("status");
+                    if (status.equalsIgnoreCase("done")) {
                         Toast toast = Toast.makeText(this, "Signature capture successful!", Toast.LENGTH_SHORT);
                         toast.setGravity(Gravity.TOP, 105, 50);
                         toast.show();
@@ -44,5 +43,4 @@ public class CaptureSignatureActivity extends Activity {
         }
 
     }
-
 }
