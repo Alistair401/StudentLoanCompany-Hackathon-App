3908383a2b7cabe8ec5767aa2e10ffb3f718c8dc
Alistair Miles <ajrmiles@gmail.com>
2017-10-28 18:53:24 +0100

    Removed input titles in favour of hints, form is filled out with uni data

--- App/app/src/main/java/guts/carpaltunnel/mobileapp/UniversityActivity.java
+++ App/app/src/main/java/guts/carpaltunnel/mobileapp/UniversityActivity.java
@@ -15,45 +15,47 @@ import guts.carpaltunnel.mobileapp.util.FormManager;
  */
 
 public class UniversityActivity extends AppCompatActivity {
-    String[] universities = { "University of Glasgow" ,"University of Edinburgh", "University of Aberdeen",
-                            "Abertay University", "Bangor University", "University of Bath",
-                            "University of Birmingham","University of Bristol","University of Cambridge",
-                            "University of Dundee", "Imperial College London", "University of Leeds",
-                            "University of Leicester", "University of London", "University of Liverpool",
-                            "University of Manchester", "University of Nottingham", "University of Oxford",
-                            "The Robert Gordon University", "University of St Andrews", "University of Stirling",
-                            "University of Strathclyde", "University of Sunderland", "University of the Arts London",
-                            "University of Wales", "University of the West of Scotland", "University of Wolverhampton"};
+    String[] universities = {"University of Glasgow", "University of Edinburgh", "University of Aberdeen",
+            "Abertay University", "Bangor University", "University of Bath",
+            "University of Birmingham", "University of Bristol", "University of Cambridge",
+            "University of Dundee", "Imperial College London", "University of Leeds",
+            "University of Leicester", "University of London", "University of Liverpool",
+            "University of Manchester", "University of Nottingham", "University of Oxford",
+            "The Robert Gordon University", "University of St Andrews", "University of Stirling",
+            "University of Strathclyde", "University of Sunderland", "University of the Arts London",
+            "University of Wales", "University of the West of Scotland", "University of Wolverhampton"};
 
-    String [] nationalities = {"Scottish","English","Welsh","Northern Irish", "Irish national", "EU national", "National of another country"};
+    String[] nationalities = {"Scottish", "English", "Welsh", "Northern Irish", "Irish national", "EU national", "National of another country"};
 
-    String [] courses = {"Accounting and Finance","Adult and Continuing Education","American Studies",
-                        "Archaeology","Arts and Media","Informatics Astronomy","Biology and Biomedical Sciences",
-                        "Celtic Civilisation","Central and East European Studies",
-                        "Centre for Cultural Research Policy","Chemistry","Classics","Comparative Literature",
-                        "Computing Science","Creative Writing","Czech","Dentistry","Earth Sciences",
-                        "Economic and Social History","Economics","Education","Engineering",
-                        "English Language and Linguistics","English Literature","Film and Television Studies",
-                        "French","Gaelic","Geography","German","Greek","Hispanic Studies","History",
-                        "History of Art","Italian","Latin","Law","Management","Mathematics","Medicine",
-                        "Modern Languages","Music","Nankai University Collaboration","Nursing and Healthcare",
-                        "Philosophy","Physics","Polish","Politics","Psychology","Public Policy","Russian",
-                        "Scottish Literature","Slavonic Studies","Social and Political Sciences","Sociology",
-                        "Statistics","Theatre Studies","Theology and Religious Studies","Urban Studies",
-                        "Veterinary Bioscience","Veterinary Med and Surgery"};
-    /** Called when the activity is first created. */
+    String[] courses = {"Accounting and Finance", "Adult and Continuing Education", "American Studies",
+            "Archaeology", "Arts and Media", "Informatics Astronomy", "Biology and Biomedical Sciences",
+            "Celtic Civilisation", "Central and East European Studies",
+            "Centre for Cultural Research Policy", "Chemistry", "Classics", "Comparative Literature",
+            "Computing Science", "Creative Writing", "Czech", "Dentistry", "Earth Sciences",
+            "Economic and Social History", "Economics", "Education", "Engineering",
+            "English Language and Linguistics", "English Literature", "Film and Television Studies",
+            "French", "Gaelic", "Geography", "German", "Greek", "Hispanic Studies", "History",
+            "History of Art", "Italian", "Latin", "Law", "Management", "Mathematics", "Medicine",
+            "Modern Languages", "Music", "Nankai University Collaboration", "Nursing and Healthcare",
+            "Philosophy", "Physics", "Polish", "Politics", "Psychology", "Public Policy", "Russian",
+            "Scottish Literature", "Slavonic Studies", "Social and Political Sciences", "Sociology",
+            "Statistics", "Theatre Studies", "Theology and Religious Studies", "Urban Studies",
+            "Veterinary Bioscience", "Veterinary Med and Surgery"};
+    /**
+     * Called when the activity is first created.
+     */
 
     Button submit_uni;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_university);
 
 
-
         //// universities adapter
         //Create Array Adapter
-        final ArrayAdapter<String> adapter_universities = new ArrayAdapter<String>(this,android.R.layout.select_dialog_singlechoice, universities);
+        final ArrayAdapter<String> adapter_universities = new ArrayAdapter<String>(this, android.R.layout.select_dialog_singlechoice, universities);
         //Find TextView control
         final AutoCompleteTextView acTextViewUniversities = (AutoCompleteTextView) findViewById(R.id.universities);
         //Set the number of characters the user must type before the drop down list is shown
@@ -63,7 +65,7 @@ public class UniversityActivity extends AppCompatActivity {
 
         //// courses adapter
         //Create Array Adapter
-        final ArrayAdapter<String> adapter_courses = new ArrayAdapter<String>(this,android.R.layout.select_dialog_singlechoice, courses);
+        final ArrayAdapter<String> adapter_courses = new ArrayAdapter<String>(this, android.R.layout.select_dialog_singlechoice, courses);
         //Find TextView control
         final AutoCompleteTextView acTextViewCourses = (AutoCompleteTextView) findViewById(R.id.courses);
         //Set the number of characters the user must type before the drop down list is shown
@@ -73,7 +75,7 @@ public class UniversityActivity extends AppCompatActivity {
 
         //// nationalities adapter
         //Create Array Adapter
-        final ArrayAdapter<String> adapter_nationalities = new ArrayAdapter<String>(this,android.R.layout.select_dialog_singlechoice, nationalities);
+        final ArrayAdapter<String> adapter_nationalities = new ArrayAdapter<String>(this, android.R.layout.select_dialog_singlechoice, nationalities);
         //Find TextView control
         final AutoCompleteTextView acTextViewNationalities = (AutoCompleteTextView) findViewById(R.id.nationalities);
         //Set the number of characters the user must type before the drop down list is shown
@@ -86,10 +88,10 @@ public class UniversityActivity extends AppCompatActivity {
         submit_uni.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent myIntent = new Intent(view.getContext(), BankActivity.class);
-                FormManager formManager = ((HSApplication)getApplicationContext()).formManager;
-                formManager.setField("university",adapter_universities.getItem(acTextViewUniversities.getListSelection()));
-                formManager.setField("nationality",adapter_nationalities.getItem(acTextViewNationalities.getListSelection()));
-                formManager.setField("course",adapter_courses.getItem(acTextViewCourses.getListSelection()));
+                FormManager formManager = ((HSApplication) getApplicationContext()).formManager;
+                formManager.setField("university", acTextViewUniversities.getText().toString());
+                formManager.setField("nationality", acTextViewNationalities.getText().toString());
+                formManager.setField("course", acTextViewCourses.getText().toString());
                 startActivityForResult(myIntent, 0);
             }
         });
