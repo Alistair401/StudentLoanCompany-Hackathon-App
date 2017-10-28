bd512d5ba2bf47a4057e8ed5c0a0cf4391c03ae2
Alistair Miles <ajrmiles@gmail.com>
2017-10-28 16:45:46 +0100

    Now supports deploying to aws. Includes most if not all fields we need to save

--- RestService/server.js
+++ RestService/server.js
@@ -5,9 +5,9 @@ var sqlite3 = require('sqlite3');
 
 var router = express.Router();
 
-let port = 8080;
+let port = process.env.PORT || 80;
 let db = new sqlite3.Database(':memory:', sqlite3.OPEN_READWRITE, function () {
-    db.run("CREATE TABLE submissions (name TEXT,birthday TEXT,email TEXT,university TEXT,nationality TEXT,perm_address TEXT)");
+    db.run("CREATE TABLE submissions (name TEXT,birthday TEXT,email TEXT,university TEXT,nationality TEXT,perm_address TEXT, signature BLOB, image BLOB, course TEXT, degree_year INTEGER, bank_number INTEGER, sort_code TEXT, borrow_amount INTEGER)");
 });
 
 app.use(bodyParser.json());
@@ -25,13 +25,22 @@ router.route('/submit').post(function (req, res) {
     });
 });
 
+router.route('/retrieve').get(function (req, res) {
+    console.log('DEBUG: accessed \'/retrieve\'');
+    console.log('DEBUG: returning all submissions');
+    db.all('SELECT * FROM submissions', function (err, rows) {
+        res.json(rows)
+    });
+});
+
 app.use('/', router);
 
-app.listen(port);
-console.log('DEBUG: app started on port ' + port);
+app.listen(port, function () {
+    console.log('DEBUG: app started on port ' + port);
+});
 
 function submit(form_obj, callback) {
-    db.run('INSERT into submissions VALUES (?,?,?,?,?,?)', [form_obj['name'], form_obj['birthday'], form_obj['email'], form_obj['university'], form_obj['nationality'], form_obj['perm_address']], function (err) {
+    db.run('INSERT into submissions VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)', [form_obj['name'], form_obj['birthday'], form_obj['email'], form_obj['university'], form_obj['nationality'], form_obj['perm_address'], form_obj['signature'], form_obj['image'],form_obj['course'],form_obj['degree_year'],form_obj['bank_number'],form_obj['sort_code'],form_obj['borrow_amount'] ], function (err) {
         if (err) {
             console.log('DEBUG: error');
         } else {
