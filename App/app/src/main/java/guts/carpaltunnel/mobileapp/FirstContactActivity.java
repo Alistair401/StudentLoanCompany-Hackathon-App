271be20a4ee1c449093139ce158045c299cec3f2
Tom Gough <tomgough96@gmail.com>
2017-10-28 19:39:59 +0100

    added relationship to you

--- App/app/src/main/java/guts/carpaltunnel/mobileapp/FirstContactActivity.java
+++ App/app/src/main/java/guts/carpaltunnel/mobileapp/FirstContactActivity.java
@@ -8,8 +8,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class FirstContactActivity extends AppCompatActivity {
@@ -28,6 +30,16 @@ public class FirstContactActivity extends AppCompatActivity {
         submit_form = findViewById(R.id.submit_form);
         contact_name = findViewById(R.id.contact_name);
         contact_mobile = findViewById(R.id.contact_mobile);
+
+        //get the spinner from the xml.
+        Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
+        //create a list of items for the spinner.
+        String [] relation = {"Mother","Father","Daughter","Son","Brother","Sister","Uncle","Auntie","Grandfather","Grandmother","Other"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,relation);
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
 
         import_contact.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
