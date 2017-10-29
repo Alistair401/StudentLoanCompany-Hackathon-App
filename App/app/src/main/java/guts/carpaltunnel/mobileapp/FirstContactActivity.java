4a75690c0e4cf07fc0688d177d9e9326873600cf
Alex <alex.j.hepburn@gmail.com>
2017-10-29 11:36:28 +0000

    Toolbars added: FirstContact + University

--- App/app/src/main/java/guts/carpaltunnel/mobileapp/FirstContactActivity.java
+++ App/app/src/main/java/guts/carpaltunnel/mobileapp/FirstContactActivity.java
@@ -1,5 +1,6 @@
 package guts.carpaltunnel.mobileapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -7,7 +8,10 @@ import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -19,11 +23,15 @@ import guts.carpaltunnel.mobileapp.util.FormManager;
 
 public class FirstContactActivity extends AppCompatActivity {
 
+    Activity ctx = this;
+
     public static final int CONTACT_REQUEST = 1;
 
     Button submit_form;
     EditText contact_name, contact_mobile;
     FloatingActionButton import_contact;
+
+    private Toolbar mTopToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +71,9 @@ public class FirstContactActivity extends AppCompatActivity {
                 startActivityForResult(myIntent, 0);
             }
         });
+
+        mTopToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(mTopToolbar);
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -84,5 +95,30 @@ public class FirstContactActivity extends AppCompatActivity {
                 contact_name.setText(name);
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent myIntent = new Intent(ctx, ChatbotActivity.class);
+            startActivityForResult(myIntent, 0);
+//            Toast.makeText(WelcomeActivity.this, "Action clicked", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
